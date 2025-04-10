75a36b089b8a24d2421e712935643b6c6279f78d
Kuhu-tech <kanchanvishwakarma200416@gmail.com>
2025-04-10 16:27:13 +0530

    todoooo

---
+++ index.js
@@ -0,0 +1,6 @@
+function addTask(){
+    const newTask = document.createElement('li')
+    // const tasklist = document.getElementsById('#tasklist')
+        tasklist.appendChild(newTask)
+   console.log(newTask)
+}
